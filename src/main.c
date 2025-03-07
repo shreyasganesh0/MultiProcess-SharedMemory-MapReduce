78e7d935252dd0a0a6dc98d87b4730511dfebe23
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-03-07 09:35:14 -0500

    Add some paging logic for the shared memory used in the buffers

--- src/main.c
+++ src/main.c
@@ -25,7 +25,25 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    comm_buf = (comm_buf_t *)malloc(num_users * sizeof(comm_buf_t));
+    comm_buf = mmap(NULL, num_users * sizeof(comm_buf_t), PROT_READ|PROT_WRITE, MAP_ANON|MAP_SHARED, -1, 0);
+    tuple_buf_block = mmap(NULL, num_users * sizeof(tuple_t) * num_slots, PROT_READ|PROT_WRITE, MAP_ANON|MAP_SHARED, -1, 0);
+
+    for (int i = 0; i < num_users; i++) {
+        sem_init(&comm_buf[i].empty, 0, num_slots);
+        sem_init(&comm_buf[i].full, 0, 0);
+        comm_buf[i].tuple_buf = tuple_buf_block[i * num_slots]; 
+        if (comm_buf[i].tuple_buf == NULL){
+            printf("Failure to allocate space for buffer\n");
+            return -1;
+        }
+        pthread_mutex_init(&comm_buf[i].mutex, NULL);
+        comm_buf[i].in_buf_loc = 0;
+        comm_buf[i].taken = 0;
+        comm_buf[i].out_buf_loc = 0;
+        comm_buf[i].topic_score_map = create_hashmap(INITIAL_CAPACITY);
+	    strcpy(comm_buf[i].userID, "x");
+
+    }
 
     int forkids = malloc(num_users * sizeof(int));
 
@@ -49,28 +67,6 @@ int main(int argc, char *argv[]) {
     }
     
 
-    for (int i = 0; i < num_users; i++) {
-        sem_init(&comm_buf[i].empty, 0, num_slots);
-        sem_init(&comm_buf[i].full, 0, 0);
-        comm_buf[i].tuple_buf = (tuple_t *)calloc(num_slots, sizeof(tuple_t));
-        if (comm_buf[i].tuple_buf == NULL){
-            printf("Failure to allocate space for buffer\n");
-            return -1;
-        }
-        pthread_mutex_init(&comm_buf[i].mutex, NULL);
-        comm_buf[i].in_buf_loc = 0;
-        comm_buf[i].taken = 0;
-        comm_buf[i].out_buf_loc = 0;
-        comm_buf[i].topic_score_map = create_hashmap(INITIAL_CAPACITY);
-	    strcpy(comm_buf[i].userID, "x");
-
-        red_idxs[i] = i;
-        int pred_err = pthread_create(&reducer_tids[i], NULL, reducer, &red_idxs[i]);
-        if (pred_err != 0) {
-            printf("Thread creation failure\n");
-            return -1;
-        }
-    }
 
 
     return 0;
